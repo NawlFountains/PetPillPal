544ff27749c0647cd9b4d09aab11413a586b6d9f
Nahuel <nahuelfuentes2001@gmail.com>
2026-05-25 01:01:33 -0300

    build: fix stub supabase web telemetry in metro

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
-const { getDefaultConfig } = require('expo/metro-config')
-const { withNativeWind } = require('nativewind/metro')
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css'})
+// Tell Metro to map the missing web telemetry package to an empty module 
+// instead of crashing the native Android bundler
+config.resolver.emptyModulesProxy = '@opentelemetry/api';
+
+module.exports = withNativeWind(config, { input: './global.css' });

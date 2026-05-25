a2948b014bb4e62f19358455e6591fc9135c5bcc
Nahuel <nahuelfuentes2001@gmail.com>
2026-05-25 01:16:39 -0300

    build: downgrade supabase to drop opentelemetry

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,15 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('expo/metro-config')
+const { withNativeWind } = require('nativewind/metro')
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-// Tell Metro to map the missing web telemetry package to an empty module 
-// instead of crashing the native Android bundler
-config.resolver.emptyModulesProxy = '@opentelemetry/api';
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === '@opentelemetry/api') {
+    return {
+      type: 'empty',
+    }
+  }
+  return context.resolveRequest(context, moduleName, platform)
+}
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, { input: './global.css' })

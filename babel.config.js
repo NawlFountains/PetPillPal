23766ebec6a12a44e854465139b99c06da1220ea
Nahuel <nahuelfuentes2001@gmail.com>
2026-05-25 00:56:34 -0300

    build: merge babel dynamic import fix

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,13 @@
 module.exports = function(api) {
-  api.cache(true)
+  api.cache(true);
   return {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
-  }
-}
+    plugins: [
+      // Translates Supabase's dynamic web-imports into static code for Hermes
+      'babel-plugin-transform-dynamic-import'
+    ],
+  };
+};

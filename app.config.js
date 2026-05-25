65a4084025adb421fb7d80079131db3898a4a544
Nahuel <nahuelfuentes2001@gmail.com>
2026-05-25 00:41:44 -0300

    ci: dynamic app.config.js with fallback to local file

---
+++ app.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  expo: {
+    name: "PetMeds",
+    slug: "PetMeds",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "petmeds",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.nawl.PetMeds"
+    },
+    android: {
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON || "./google-services.json",
+      softwareKeyboardLayoutMode: "pan",
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png"
+      },
+      predictiveBackGestureEnabled: false,
+      package: "com.nawl.PetMeds"
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router"
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "36f15f60-848d-4f55-99c4-98306f6967e4"
+      }
+    }
+  }
+};

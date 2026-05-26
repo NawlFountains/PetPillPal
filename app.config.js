aa6fc2e017d91088b93a6b9727baa73425133305
Nahuel <nahuelfuentes2001@gmail.com>
2026-05-26 19:41:10 -0300

    Updated version number

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "PetMeds",
     slug: "PetMeds",
-    version: "1.0.0",
+    version: "1.1.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "petmeds",
